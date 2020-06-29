6443ea2929b11da5aa4db136c7748be65bb5647b
Norman Witte III <norm.witte@gmail.com>
2020-06-28 22:19:25 -0400

    Create 6 actions with a 3 second delay

--- sockets/action1.js
+++ sockets/action1.js
@@ -1,15 +1,17 @@
 const AWS = require('aws-sdk');
 
 module.exports = async (event, context, callback) => {
-  const message = JSON.parse(event.body).data;
+  const { data } = JSON.parse(event.body);
   const websocket = new AWS.ApiGatewayManagementApi({
     apiVersion: '2018-11-29',
     endpoint: `${event.requestContext.domainName}/${event.requestContext.stage}`,
   });
 
+  await new Promise((resolve) => this.setTimeout(resolve, 3000));
+
   await websocket.postToConnection({
     ConnectionId: event.requestContext.connectionId,
-    Data: message,
+    Data: data,
   }).promise();
 
   return callback(null, {
